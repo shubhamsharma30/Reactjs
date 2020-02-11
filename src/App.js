c16be19e5e38185ceffa6724d33acbc50c52edff
shubhamsharma30 <ss1008042@gmail.com>
2020-02-11 18:16:17 +0530

    added dragdrop container

--- src/App.js
+++ src/App.js
@@ -1,49 +1,7 @@
-import React, {useState } from 'react';
-import './App.css';
-import Person from './person/person';
+import DragDropContainer from './DragDropContainer';
+import DropTarget from './DropTarget';
 
-const App= props =>{
+module.exports = {
+  DragDropContainer, DropTarget,
+};
 
-  const[personstate,setpersonstate]=useState({
-
-  Persons:[
-    {id:"1.", name:"shubham"},
-    {id:"2." ,name:"sagar"},
-    {id:"3.",name:"sachin"}
-  ]
-});
-
-const clickHandler=() =>{
-  setpersonstate({
-    Persons:[
-      {id:"1.", name:"Shubham"},
-      {id:"2." ,name:"sourav"},
-      {id:"4.",name:"Neymar"}
-    ] 
-  });
-}
-
-    return (
-     
-      <div className="App">
-        <button onClick={clickHandler}>Click it</button>
-        <Person
-            id={personstate.Persons[0].id} 
-            name={personstate.Persons[0].name}>
-        </Person>
-        <Person
-             id={personstate.Persons[1].id}
-             name={personstate.Persons[1].name}
-             click={clickHandler}>
-        </Person>
-        <Person 
-             id={personstate.Persons[2].id}
-             name={personstate.Persons[2].name}>
-        </Person>
-         <h1>Devops stands for development and operations</h1>
-      </div>
-    );
-  }  
-
-
-export default App;
